937bb8d6143ad377e99fd70529075b3e4c4d9426
Luiz Winter Jr <77393235+luizwinter@users.noreply.github.com>
2021-03-02 16:17:37 -0300

    Adicionando funções aos botões.

--- scripts/controler/calccontroler.js
+++ scripts/controler/calccontroler.js
@@ -1,6 +1,7 @@
 class CalcControler{
 
     constructor(){
+        this._operation = [];
         this._displayEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
@@ -26,20 +27,91 @@ class CalcControler{
             element.addEventListener(event, fn, false);
         });
     }
+    
+    
+    addOperation(value){
+        this._operation.push(value);
+    }
+    
+    clearAll(){
+        this._operation = [];
+    }
+        cancelEntry(){
+        this._operation.pop();
+    }
+    setError(){
+        this._displayEl = "ERROR";
+    }
+    
+    
+    execBtn(value){
+        switch(value){
+            case 'ac':
+                this.clearAll();
+                break;
+            case 'ce':
+                this.cancelEntry();
+                console.log(this._operation);
+                break;
+            case 'porcento':
+                console.log(this._operation);
+                break;
+            case 'soma':
+                console.log(this._operation);
+                break;
+            case 'divisao':
+                console.log(this._operation);
+                break;
+            case 'multiplicacao':
+                console.log(this._operation);
+            break;
+            case 'subtracao':
+                console.log(this._operation);
+                break;
+            case 'igual':
+                console.log(this._operation);
+                break;
+            case 'ponto':
+                console.log(this._operation);
+                break;
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            case '0':
+                this.addOperation(parseInt(value));
+                console.log(this._operation)
+                
+                
+                  
+            default:
+            this.setError();                
+            break;
+        }}
+    
+    
 
     initButtonsEvents(){
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
         buttons.forEach((btn, index) =>{
             this.addEventListenerAll(btn, "click drag", e =>{
-                console.log(btn.className.baseVal.replace("btn-", ""));
+                let textBtn = btn.className.baseVal.replace("btn-", "");
+                console.log(textBtn) ;
+                
+                this.execBtn(textBtn);
             });
 
             this.addEventListenerAll(btn, "mouseover mouseup mousedown", e=>{
                 btn.style.cursor = "pointer";
-            })
+            });
 
 
-        })
+        });
         
     }
 
